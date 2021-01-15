4cb8ed4cc8b2d839eb65cc88cf395b7f69b54dfa
Christa O <68796545+oshodic@users.noreply.github.com>
2021-01-15 16:28:31 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -11,17 +11,18 @@ searchForm.addEventListener("submit", function(e){
   e.preventDefault();
 
   newGif = searchBar.value;
+  const response = makeRequest(newGif);
 
-  async function makeRequest(newGif) {
-    const getMeme = await axios.get('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC', { params: {newGif} });
-    
-    return getMeme.data.id;
-  }
-
-  appendGifs.append(makeRequest(newGif));
+  appendGifs.append(response);
   searchBar.value = '';
 
 });
+
+async function makeRequest(newGif) {
+  const getMeme = await axios.get('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC', { params: {newGif} });
+  
+  return getMeme;
+}
 
 
 
